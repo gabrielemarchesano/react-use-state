da8599eae1f19de6fe5d6b114bf67f21e6401469
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-07 16:55:08 +0100

    create reactivity to the cards by clicking the buttons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function Nav({ languages }) {
 
-  const [id, setId] = useState(0);
+  const [id, setId] = useState(languages[0].id);
 
   return (
     <div>
@@ -18,10 +18,10 @@ export default function Nav({ languages }) {
       <div className="card">
         <div className="card-body">
           <div className="card-title">
-            <h3>{languages[id].title}</h3>
+            <h3>{languages.find(item => item.id === id).title}</h3>
           </div>
           <div className="card-text">
-            <p>{languages[id].description}</p>
+            <p>{languages.find(item => item.id === id).description}</p>
           </div>
         </div>
       </div>
